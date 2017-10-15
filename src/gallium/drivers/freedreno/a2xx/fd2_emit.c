laanwj/mesa-1

a2xx: Prevent assertion error in emit_texture if view is not set
Textures will also be updated if texture view state was un-set,
without this change that causes an assertion crash.

Signed-off-by: Wladimir J. van der Laan <laanwj@gmail.com>

src/gallium/drivers/freedreno/a2xx/fd2_emit.c
@@ -127,6 +127,7 @@ emit_texture(struct fd_ringbuffer *ring, struct fd_context *ctx,
 {
 	unsigned const_idx = fd2_get_const_idx(ctx, tex, samp_id);
 	static const struct fd2_sampler_stateobj dummy_sampler = {};
+	static const struct fd2_sampler_stateobj dummy_view = {};
 	const struct fd2_sampler_stateobj *sampler;
 	struct fd2_pipe_sampler_view *view;
 
@@ -136,16 +137,19 @@ emit_texture(struct fd_ringbuffer *ring, struct fd_context *ctx,
 	sampler = tex->samplers[samp_id] ?
 			fd2_sampler_stateobj(tex->samplers[samp_id]) :
 			&dummy_sampler;
-	view = fd2_pipe_sampler_view(tex->textures[samp_id]);
+	view = tex->textures[samp_id] ?
+			fd2_pipe_sampler_view(tex->textures[samp_id]) :
+			&dummy_view;
 
 	OUT_PKT3(ring, CP_SET_CONSTANT, 7);
 	OUT_RING(ring, 0x00010000 + (0x6 * const_idx));
 
-        assert(sampler);
-        assert(view);
-
 	OUT_RING(ring, sampler->tex0 | view->tex0);
-	OUT_RELOC(ring, fd_resource(view->base.texture)->bo, 0, view->fmt, 0);
+	if (view->base.texture)
+		OUT_RELOC(ring, fd_resource(view->base.texture)->bo, 0, view->fmt, 0);
+	else
+		OUT_RING(ring, 0);
+
 	OUT_RING(ring, view->tex2);
 	OUT_RING(ring, sampler->tex3 | view->tex3);
 	OUT_RING(ring, sampler->tex4);

imirkin/mesa

src/gallium/drivers/freedreno/a2xx/fd2_emit.c
@@ -127,6 +127,7 @@ emit_texture(struct fd_ringbuffer *ring, struct fd_context *ctx,
 {
 	unsigned const_idx = fd2_get_const_idx(ctx, tex, samp_id);
 	static const struct fd2_sampler_stateobj dummy_sampler = {};
+	static const struct fd2_pipe_sampler_view dummy_view = {};
 	const struct fd2_sampler_stateobj *sampler;
 	struct fd2_pipe_sampler_view *view;
 
@@ -136,16 +137,19 @@ emit_texture(struct fd_ringbuffer *ring, struct fd_context *ctx,
 	sampler = tex->samplers[samp_id] ?
 			fd2_sampler_stateobj(tex->samplers[samp_id]) :
 			&dummy_sampler;
-	view = fd2_pipe_sampler_view(tex->textures[samp_id]);
+	view = tex->textures[samp_id] ?
+			fd2_pipe_sampler_view(tex->textures[samp_id]) :
+			&dummy_view;
 
 	OUT_PKT3(ring, CP_SET_CONSTANT, 7);
 	OUT_RING(ring, 0x00010000 + (0x6 * const_idx));
 
-        assert(sampler);
-        assert(view);
-
 	OUT_RING(ring, sampler->tex0 | view->tex0);
-	OUT_RELOC(ring, fd_resource(view->base.texture)->bo, 0, view->fmt, 0);
+	if (view->base.texture)
+		OUT_RELOC(ring, fd_resource(view->base.texture)->bo, 0, view->fmt, 0);
+	else
+		OUT_RING(ring, 0);
+
 	OUT_RING(ring, view->tex2);
 	OUT_RING(ring, sampler->tex3 | view->tex3);
 	OUT_RING(ring, sampler->tex4);